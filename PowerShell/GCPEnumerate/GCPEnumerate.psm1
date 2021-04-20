3e44508a5343b397c966dee034af6d451537a471
Robert Teller <robert.j.c.teller@gmail.com>
2021-04-20 10:18:48 -0700

    v0.01 of subnetUtilization

--- PowerShell/GCPEnumerate/GCPEnumerate.psm1
+++ PowerShell/GCPEnumerate/GCPEnumerate.psm1
@@ -1,8 +1,115 @@
+#https://codeandkeep.com/PowerShell-Get-Subnet-NetworkID/
+
+class Instance {
+    [string] $name
+    [string] $network
+    [string] $networkIp
+    [string] $state
+    [string] $projectId
+}
+
+class Subnet {
+    [string] $name
+    [string] $ipCidrRange="255.255.255.255/32"
+    [Instance[]] $instances = @()
+    [string] $region
+    hidden [int64] $maxHosts = [math]::Pow(2,(32 - $this.ipCidrRange.split("/")[1] )) - 2
+    hidden [int64] $usedHosts = $this.instances.count
+    hidden [string] $percentUsed
+    [psobject] ShowUtilization(){
+        $_ShowUtilization = New-Object -TypeName PSObject 
+        $utilization = @{
+            InputObject=$_ShowUtilization;
+            MemberType='NoteProperty';
+        }
+        $this.maxHosts = [math]::Pow(2,(32 - $this.ipCidrRange.split("/")[1] )) - 2
+        $this.usedHosts = $this.instances.count
+        $this.percentUsed = ($this.usedHosts / $this.maxHosts).ToString("P")
+        
+        Add-Member @utilization -Name maxHosts -Value $this.maxHosts
+        Add-Member @utilization -Name usedHosts -Value $this.usedHosts
+        Add-Member @utilization -Name percentUsed -Value $this.percentUsed
+        return $_ShowUtilization
+    }
+}
+
+class Network {
+    [string] $name
+    [Subnet[]] $subnets = @()
+}
+
+function ConvertFrom-CIDR {
+    <#
+    .SYNOPSIS
+    Converts a number of bits (0-32) to an IPv4 network mask string (e.g., "255.255.255.0").
+  
+    .DESCRIPTION
+    Converts a number of bits (0-32) to an IPv4 network mask string (e.g., "255.255.255.0").
+  
+    .PARAMETER MaskBits
+    Specifies the number of bits in the mask.
+    #>
+    param(
+      [parameter(Mandatory=$true)]
+      [ValidateRange(0,32)]
+      [Int] $MaskBits
+    )
+    $mask = ([Math]::Pow(2, $MaskBits) - 1) * [Math]::Pow(2, (32 - $MaskBits))
+    $bytes = [BitConverter]::GetBytes([UInt32] $mask)
+    (($bytes.Count - 1)..0 | ForEach-Object { [String] $bytes[$_] }) -join "."
+}
+
+function Get-BroadcastAddress {
+    param(
+        [string] $subnet,
+        [string] $subnetMask
+    )
+
+    filter Convert-IP2Decimal {
+        ([IPAddress][String]([IPAddress]$_)).Address
+    }
+
+
+    filter Convert-Decimal2IP {
+        ([System.Net.IPAddress]$_).IPAddressToString 
+    }
+
+    [UInt32]$_ip = $subnet | Convert-IP2Decimal
+    [UInt32]$_subnet = $subnetMask | Convert-IP2Decimal
+    [UInt32]$_broadcast = $_ip -band $_subnet 
+    $_broadcast -bor -bnot $_subnet | Convert-Decimal2IP
+}
+
+function CheckAddressRange {
+    param(
+        [string] $ipCidrRange,
+        [string] $networkIp
+    )
+
+    $subnet,$cidr = $ipCidrRange.split("/")
+    $subnetMask = ConvertFrom-CIDR -MaskBits $cidr
+    $broadcast = Get-BroadcastAddress -subnet $subnet -subnetMask $subnetMask
+
+    $_start = [ipaddress]::Parse($subnet).GetAddressBytes()
+    [array]::Reverse($_start)
+    $_start_int = [BitConverter]::ToUInt32($_start,0)
+
+    $_end = [ipaddress]::Parse($broadcast).GetAddressBytes()
+    [array]::Reverse($_end)
+    $_end_int = [BitConverter]::ToUInt32($_end,0)
+
+    $_ip = [ipaddress]::Parse($networkIp).GetAddressBytes()
+    [array]::Reverse($_ip)
+    $_ip_int = [BitConverter]::ToUInt32($_ip,0)
+
+    $_start_int -le $_ip_int -and $_end_int -ge $_ip_int
+}
+
+
 function New-GCPSession {
     param (
         [string]$OrganizationId,
-        [string]$HostProjectId,
-        [switch]$NoPrompt = $false
+        [string]$HostProjectId
     )
 
 
@@ -11,15 +118,16 @@ function New-GCPSession {
     # Set-Variable -Name Projects -Value (New-ProjectLookup -OrganizationId $selectedOrg) -Scope Global
     #Set-Variable -Name OrgId -Value $selectedOrg -Scope Global
     $OrgId, $DefaultDomainName = Select-Organization -OrganizationId $OrganizationId
-    $XpnID = Select-HostProject -HostProjectId $HostProjectId
     Set-Variable -Name OrgId -Value $OrgId -Scope Global
     Set-Variable -Name DefaultDomainName -Value $DefaultDomainName -Scope Global
+    
+    $XpnID = Select-HostProject -HostProjectId $HostProjectId
     Set-Variable -Name XpnID -value $XpnID -Scope Global
 }
 
 function Select-Organization {
     param (
-        [string]$OrganizationId = ""
+        [string]$OrganizationId
     )
 
     $orgs = Get-Organizations
@@ -32,7 +140,7 @@ function Select-Organization {
         $domainName = ""
     }
     
-    while ($True) {
+    while (!$selectedOrg) {
         Write-Host -ForegroundColor Green "[+] Select a organization: `n"
         for ($i = 0 ; $i -lt $orgs.Count; $i++) {
             Write-Host -ForegroundColor Green "[$i] " -NoNewline
@@ -43,7 +151,6 @@ function Select-Organization {
         Write-Host
         $selection = Read-Host
         if ($selection -eq "q") {
-            Clear-Host
             Write-Error -Message "Error: User decided to exit prompt" -Category CloseError -ErrorAction Stop
         } elseif ($orgs.Count -le $selection ) {
             Clear-Host
@@ -51,34 +158,11 @@ function Select-Organization {
             Start-Sleep 3
             Clear-Host
             continue
+        } else {
+            $selectedOrg = $orgs[$selection].name.Split("/")[1]
+            $domainName = $orgs[$selection].displayName
+            break
         }
-        
-        while ($true) {
-            Clear-Host
-            $confirm = @"
-Confirm selection y/n or q to Quit
-    Selected org: $($orgs[$selection].displayName)
-"@
-            $confirm = Read-Host -Prompt $confirm
-            if ($confirm -eq "y") {
-                $selectedOrg = $orgs[$selection].name.Split("/")[1]
-                $domainName = $orgs[$selection].displayName
-                break
-            }
-            elseif ($confirm -eq "n") {
-            Clear-Host
-                break
-            }
-            elseif ($confirm -eq "q") {
-                Write-Error -Message "Error: User decided to exit prompt" -Category CloseError -ErrorAction Stop
-            }
-            Clear-Host
-            Write-Host -ForegroundColor Red "`nIncorrect key [$confirm] pressed, Please confirm selection with y or n."
-            Start-Sleep 3
-            Clear-Host
-            continue
-        }
-        if ($selectedOrg) {break}
     }
     return  $selectedOrg, $domainName
 }
@@ -96,7 +180,7 @@ function Select-HostProject {
         $selectedXPN = ""
     }
 
-    while ($True) {
+    while (!$selectedXPN) {
         Write-Host -ForegroundColor Green "[+] Select a Host Project: `n"
         for ($i = 0 ; $i -lt $xpns.Count; $i++) {
             Write-Host -ForegroundColor Green "[$i] " -NoNewline
@@ -107,97 +191,162 @@ function Select-HostProject {
         Write-Host
         $selection = Read-Host
         if ($selection -eq "q") {
-            Clear-Host
             Write-Error -Message "Error: User decided to exit prompt" -Category CloseError -ErrorAction Stop
-        } elseif ($orgs.Count -le $selection ) {
+        } elseif ($xpns.Count -le $selection ) {
             Clear-Host
-            Write-Host -ForegroundColor Red "`nProvide a proper selection, [$selection] is not within range 0 - $($orgs.Count-1)"
+            Write-Host -ForegroundColor Red "`nProvide a proper selection, [$selection] is not within range 0 - $($xpns.Count-1)"
             Start-Sleep 3
             Clear-Host
             continue
-        }
-
-        while ($true) {
-            Clear-Host
-            $confirm = @"
-Confirm selection y/n or q to Quit
-    Selected Host Project: $($xpns[$selection].name)
-"@
-            $confirm = Read-Host -Prompt $confirm
-            if ($confirm -eq "y") {
-                $selectedXPN = $xpns[$selection].name
-                break
-            }
-            elseif ($confirm -eq "n") {
-            Clear-Host
-                break
-            }
-            elseif ($confirm -eq "q") {
-                Write-Error -Message "Error: User decided to exit prompt" -Category CloseError -ErrorAction Stop
-            }
-            Clear-Host
-            Write-Host -ForegroundColor Red "`nIncorrect key [$confirm] pressed, Please confirm selection with y or n."
-            Start-Sleep 3
-            Clear-Host
-            continue
-        }
-        if ($selectedXPN) {break}        
+        } else {
+            $selectedXPN = $xpns[$selection].name
+        }       
     }
     return $selectedXPN
 }
 
 function Get-Organizations {
-    $orgs = gcloud organizations list --format=json | ConvertFrom-Json
+    $orgs = @(gcloud organizations list --format=json | ConvertFrom-Json)
 
     $orgs
 }
 
 function Get-HostProjects {
-    <#
-    .SYNOPSIS
-    Creates in memory key value lookup where key is projectid and value is project display name
-    
-    .DESCRIPTION
-    This function uses the asset inventory api to dump all host projects in the entire organization
-
-    .PARAMETER OrganizationId
-    GCP Organization ID 
-    
-    .EXAMPLE
-    $projects = Get-HostProjects -OrganizationId "123456789"
-    
-    .NOTES
-    Access project name: $projects["1097427954669"]  
-    #>
     param (
         [string]$OrganizationId = $Global:OrgId
     )
 
-    $hostProjects = gcloud compute shared-vpc organizations list-host-projects $OrganizationId `
-        --page-size=500 `
-        --format=json `
-        | ConvertFrom-Json
-
+    $hostProjects = @(gcloud compute shared-vpc organizations list-host-projects $OrganizationId --format=json | ConvertFrom-Json)
     $hostProjects
 }
 
+function Get-ServiceProjects {
+    param (
+        [string]$HostProjectId  = $Global:XpnId
+    )
+
+    $serviceProjects = @(gcloud compute shared-vpc list-associated-resources $HostProjectId --format=json | ConvertFrom-Json)
+    $serviceProjects
+}
+
+function Get-Subnets {
+    param (
+        [string]$HostProjectId  = $Global:XpnId
+    )
+    $subnets = @(gcloud compute networks subnets list --project=$HostProjectId --format=json | ConvertFrom-Json)
+    $subnets
+}
+
+function Get-Networks {
+    param (
+        [string]$HostProjectId  = $Global:XpnId
+    )
+    $networks = @(gcloud compute networks list --project=$HostProjectId --format=json | ConvertFrom-Json)
+    $networks
+}
+
+function Show-SubnetUtilization {
+    $_subnetUtilization = Get-SubnetUtilization
+        
+    foreach ($network in $_subnetUtilization.keys) {
+        Write-Host -ForegroundColor Green "VPC Network: "  -NoNewline
+        Write-Host $network
+        foreach ($subnet in $_subnetUtilization[$network].subnets) {
+            Write-Host -ForegroundColor Red "`tSubnetwork: "  -NoNewline
+            Write-Host "$($subnet.name) - $($subnet.ipCidrRange) [$($subnet.ShowUtilization().percentUsed)]"
+        }
+    }
+}
+function Get-SubnetUtilization {
+    $_subnets = Get-Subnets
+    $_networks = Get-networks
+    $_serviceProjects = Get-ServiceProjects
+    
+    $networks = @{}
+    foreach($_network in $_networks){
+        $network = [Network]::new()
+        $network.name = $_network.name
+        $networks.add($_network.name,$network)
+    }
+    foreach($_subnet in $_subnets) {
+        
+        $subnet = [Subnet]::new()
+        
+        $subnet.name = $_subnet.name
+        $subnet.region = $_subnet.region.split("/")[-1]
+        $subnet.ipCidrRange = $_subnet.ipCidrRange
+        $_subnet_network = $_subnet.network.split("/")[-1]
+        $networks[$_subnet_network].subnets += $subnet | Add-Member index ($networks[$_subnet_network].subnets.count) -PassThru
+    }
+
+    foreach ($_project in $_serviceProjects) {
+        $_instances = Get-Instances -ProjectId $_project.id
+        foreach($_instance in $_instances) {
+            foreach($_interface in @($_instance.additionalAttributes.networkInterfaces)) {
+                $instance = [Instance]::new()
+                $instance.name = $_instance.name.split("/")[-1]
+                $instance.networkIp = $_interface.networkIp
+                $instance.network =  $_interface.network.split("/")[-1]
+                $instance.projectId =  $_instance.project.split("/")[-1]
+                $instance.state = $_instance.state
+                foreach ($_subnet in $networks[$instance.network].subnets) {
+                    if (CheckAddressRange -ipCidrRange $_subnet.ipCidrRange -networkIp $instance.networkIp) { 
+                        $networks[$instance.network].subnets[$_subnet.index].instances += $instance | Add-Member index ($networks[$instance.network].subnets[$_subnet.index].instances.count) -PassThru
+                    }
+                }
+            }
+        }
+    }
+    $networks
+}
+
+function Get-Instances {
+    param (
+        [string]$ProjectId
+    )
+    $instances = @(
+        gcloud asset search-all-resources `
+        --asset-types='compute.googleapis.com/Instance' `
+        --scope="projects/$ProjectId" `
+        --format=json | ConvertFrom-Json
+    )
+    $instances
+}
+
+# To be done
 function Get-HttpProxys {
 
 }
 
+# To be done
 function Get-HttpsProxys {
-
+# gcloud asset search-all-resources \
+#   --scope='organizations/778357758552' \
+#   --query='location=us-central1' \
+#   --asset-types='compute.googleapis.com/TargetHttpsProxy,compute.googleapis.com/TargetHttpProxy'
+ 
+ 
+# gcloud asset search-all-resources --scope=organizations/778357758552 --query="location=us-central1" 
+# --asset-types="compute.googleapis.com/TargetHttpsProxy,compute.googleapis.com/TargetHttpProxy" 
+# --format="table[box,title='HTTPS LB'](displayName,project,parentFullResourceName,assetType)"
 }
 
 function Get-ForwardingRules {
 
 }
 
+# To be done
 # Exported Functions
 $Exports = @(
     "New-GCPSession"
     ,"Get-Organizations"
     ,"Get-HostProjects"
+    ,"Get-ServiceProjects"
+    ,"Get-Subnets"
+    ,"Get-Networks"
+    ,"Get-SubnetUtilization"
+    ,"Get-Instances"
+    ,"Show-SubnetUtilization"
 )
 
 Export-ModuleMember -Function $Exports
